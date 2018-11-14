d65867833911a33d9c0354c4057b55bf4965163e
Samantha brake <sammy.brake@gmail.com>
2018-11-14 00:41:06 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -76,8 +76,8 @@ end
 
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
-  Movie.create(title: "Wat?").update()
-  
+  Movie.create(title: "Wat?").update(title: "Wat, huh?")
+
 end
 
 def can_update_multiple_items_at_once
