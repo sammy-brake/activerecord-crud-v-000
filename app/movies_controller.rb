88a16b72e62a3b04c7b87265bda3f4d1d81ae51a
Samantha brake <sammy.brake@gmail.com>
2018-11-14 00:47:02 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -49,8 +49,8 @@ def can_get_size_of_the_database
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  binding.pry
-Movie.find(id = nil)
+  
+Movie.find()
 end
 
 def can_find_by_multiple_attributes
